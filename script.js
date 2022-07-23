f06a9ec9bc50b74be953a1d0f12aef973815235c
Henry Gonzalez <hegonza68@gmail.com>
2022-07-23 13:38:56 -0500

    Ejercicio Secuencial Salario Base

---
+++ script.js
@@ -0,0 +1,23 @@
+let v1,v2,v3;
+let totalPagar =0;
+let totalVentas=0;
+let salarioBase=0;
+let com=0;
+
+salarioBase= Number(prompt(`Ingrese Salario Base`));
+v1 = Number(prompt(`Ingrese venta 1`));
+v2 = Number(prompt(`Ingrese venta 2`));
+v3 = Number(prompt(`Ingrese venta 3`));
+
+totalVentas=v1+v2+v3;
+
+com = totalVentas * 0.1;
+
+totalPagar = com + salarioBase;
+
+console.log(totalVentas);
+console.log(com);
+console.log(salarioBase+com);
+
+alert(`El total a pagar para el vendedor es:` + totalPagar);
+
